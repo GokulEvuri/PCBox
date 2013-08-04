864f8e0aaa7e8b790e91c61f1842e29ca8fa8f0b
RameezHussain <rameezh88@gmail.com>
2013-08-04 23:02:50 +0200

    device discovery and list

--- PCBox_Android/src/com/quintessoft/pcbox_android/Connect.java
+++ PCBox_Android/src/com/quintessoft/pcbox_android/Connect.java
@@ -1,18 +1,26 @@
 package com.quintessoft.pcbox_android;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.Toast;
-import android.bluetooth.*;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Connect extends Activity {
 
@@ -20,6 +28,8 @@ public class Connect extends Activity {
 	
 	ListView deviceList;
 	Button connect;
+	ArrayList<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+	DeviceAdapter adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,7 @@ public class Connect extends Activity {
 		handleWidgets();
 		searchForDevices();
 		connectToDevice();
-	}
+	} 
 
 	protected void searchForDevices(){
 		
@@ -37,21 +47,27 @@ public class Connect extends Activity {
 		
 		if(bluetooth!=null){
 			
+			Log.d("BLUETOOTH", "bluetooth not null"); 
+			
 			if (bluetooth.isEnabled()) {
 			    // Enabled. Work with Bluetooth.
 				if (bluetooth.isDiscovering()) {
+					Log.d("BLUETOOTH", "is discovering"); 
 					bluetooth.cancelDiscovery();
+					devices.clear();
+					adapter.notifyDataSetChanged();
+					//deviceList.setAdapter(adapter);
 				}
 				
-				bluetooth.startDiscovery();
+				//bluetooth.startDiscovery();
 				
 				IntentFilter intent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-			    intent.addAction(BluetoothDevice.ACTION_UUID);
+			    //intent.addAction(BluetoothDevice.ACTION_UUID);
 			    intent.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
 			    intent.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
 			    
 			    final BroadcastReceiver mReceiver = new BroadcastReceiver() 
-                { 
+                {  
 			    	@Override
                     public void onReceive(Context context, Intent intent) 
                     {
@@ -59,19 +75,22 @@ public class Connect extends Activity {
                         // When discovery finds a device 
                         if (BluetoothDevice.ACTION_FOUND.equals(action)) 
                         {
-                        // Get the BluetoothDevice object from the Intent 
-                        BluetoothDevice device = intent.getParcelableExtra(
-                        BluetoothDevice.EXTRA_DEVICE);
-                        Log.v("BlueTooth Testing",device.getName() + "\n"
-                        + device.getAddress()); 
+                        	Log.d("BLUETOOTH", "here here"); 
+	                        // Get the BluetoothDevice object from the Intent 
+	                        BluetoothDevice device = intent.getParcelableExtra(
+	                        BluetoothDevice.EXTRA_DEVICE);
+	                        Log.d("BlueTooth Testing",device.getName() + "\n"
+	                        + device.getAddress());
+	                        if(!alreadyAdded(device.getAddress())){
+	                        	devices.add(device);
+		                        adapter.notifyDataSetChanged();
+	                        }
                         }
-                    }
-
-					   
+                    }  
                 };
 
-                String aDiscoverable = BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE;
-                startActivityForResult(new Intent(aDiscoverable), DISCOVERY_REQUEST);
+               // String aDiscoverable = BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE;
+               // startActivityForResult(new Intent(aDiscoverable), DISCOVERY_REQUEST);
                 IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND); 
                 registerReceiver(mReceiver, filter); 
                 bluetooth.startDiscovery();
@@ -91,8 +110,26 @@ public class Connect extends Activity {
 		
 	}
 	
+	protected boolean alreadyAdded(String address){
+		 
+		boolean isAdded = false;
+		 
+		for(BluetoothDevice device: devices){
+			
+			if(device.getAddress().matches(address)){
+				isAdded = true;
+				break;
+			}
+		}
+		 
+		return isAdded;
+	}
+	
 	protected void handleWidgets(){
 		connect = (Button) findViewById(R.id.button1);
+		deviceList = (ListView) findViewById(R.id.device_list);
+		adapter = new DeviceAdapter();
+		deviceList.setAdapter(adapter);
 	}
 
 	protected void connectToDevice(){
@@ -101,7 +138,9 @@ public class Connect extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+				devices.clear();
+				adapter.notifyDataSetChanged();
+				searchForDevices();
 			}
 		});
 	}
@@ -112,5 +151,60 @@ public class Connect extends Activity {
 		getMenuInflater().inflate(R.menu.connect, menu);
 		return true;
 	}
+	
+	private class DeviceAdapter extends BaseAdapter {
 
+		LayoutInflater inflater;
+		
+		DeviceAdapter(){
+			inflater = (LayoutInflater) Connect.this
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);	
+		}
+		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return devices.size();
+		}
+
+		@Override
+		public BluetoothDevice getItem(int position) {
+			// TODO Auto-generated method stub
+			return devices.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			convertView = inflater.inflate(R.layout.device_cell, parent, false);
+			
+			TextView txt = (TextView) convertView.findViewById(R.id.device_name);
+			
+			BluetoothDevice device = getItem(position);
+			
+			txt.setText(device.getName());
+			
+			handleDeviceSelect(convertView, position);
+			
+			return convertView;
+		}
+		
+		protected void handleDeviceSelect(View view, final int position){
+			
+			view.setOnClickListener( new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Log.d("device", getItem(position).getName()+" selected");
+				}
+			});
+		}
+	}
 }
