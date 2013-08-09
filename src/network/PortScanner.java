599d482cd2fccc6a87596d55d03fd1c7f9b1d430
Gokul <chandu.gokul138@gmail.com>
2013-08-09 11:56:12 +0200

    Portscanner functions update

--- src/network/PortScanner.java
+++ src/network/PortScanner.java
@@ -1,21 +1,66 @@
+/*
+ * Author Gokul S Evuri
+ * 
+ * ToDo List
+ * [1] Implementation to send a trace/error record to quintessoft 
+ * when all ports are busy
+ * 
+ * */
+
 package network;
 
 import java.net.*;
 
 public class PortScanner {
 
-	public void openPort(int PortNumber){
+	/*Put a bunch of unused portnumbers from wikipedia*/
+	static int [] PORT_NUM_LIST = {4449};
+	
+	/*Returns a ServerSocket object for the given PortNumber 
+	 * in case fails to open the port returns null*/
+	public ServerSocket openPort(int portNumber){
+		ServerSocket serverSock = null;
 		try  
         {  
-         ServerSocket ServerSok = new ServerSocket(PortNumber);  
-         System.out.println("Port in use: " + PortNumber );  
-         ServerSok.close();  
+         serverSock = new ServerSocket(portNumber);  
+         System.out.println("Port in use: " + portNumber );  
+         serverSock.close();  
         }  
         catch (Exception e)  
         {  
             e.printStackTrace();   
         }  
+		return serverSock;
+	}
+	
+	/*Returns a ServerSocket object using any one 
+	 * of the portnumbers array [1]*/
+	public ServerSocket openPort(int[] portNumbers,PortScanner ps){
+		ServerSocket serverSock = null;
+		boolean connectionSuccess = false;
+		int pnumlen = portNumbers.length;
+		while(!connectionSuccess){
+			if(pnumlen>0 && serverSock == null){
+				serverSock = ps.openPort(portNumbers[pnumlen-1]);
+			}
+			else {
+				connectionSuccess = true;
+			}
+			pnumlen--;
+		}
+		if(serverSock != null){
+			return serverSock;
+		}
+		else{
+			/*[1] Handle it here*/
+			return null;
+			}			
 		
+	}
+	
+	public ServerSocket openPort(){
+		PortScanner ps = new PortScanner();
+		return ps.openPort(PORT_NUM_LIST, ps);
 	}
 	
 }
